0c4678c91455cef0058b0cd42d51c4927d80cacd
Malkov <yakov.malkov@bk.ru>
2021-11-26 00:29:19 +0300

    Обработка нажатий, перемещение корабля и ограничение перемещения

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,18 +21,33 @@ class AlienInvasion:
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
-            # Отслеживание событий клавиатуры и мыши.
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        sys.exit()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
 
-                # При каждом проходе цикла перерисовывается экран.
-                self.screen.fill(self.settings.bg_color)
+    def _check_events(self):
+        # Отслеживание событий клавиатуры и мыши.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
 
-                self.ship.blitme()
-                # Отображение последнего прорисованного экрана.
-                pygame.display.flip()
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
+    def _update_screen(self):
+        """Обновляет изображения на экране и отображает новый экран."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # Отображение последнего прорисованного экрана.
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Создание экземпляра и запуск игры.
--- settings.py
+++ settings.py
@@ -6,3 +6,6 @@ class Settings():
           self.screen_width = 1200
           self.screen_height = 800
           self.bg_color = (91, 119, 172)
+
+          # Настройки корабля
+          self.ship_speed = 0.5

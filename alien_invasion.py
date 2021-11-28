f2fc527a139bb112dafd77713b0cf01e49cc9a4c
Malkov <yakov.malkov@bk.ru>
2021-11-28 16:42:39 +0300

    Стрельба, создание файла bullet.py и добавление класса Bullet

--- alien_invasion.py
+++ alien_invasion.py
@@ -64,6 +64,7 @@ class AlienInvasion:
         # Отображение последнего прорисованного экрана.
         pygame.display.flip()
 
+
 if __name__ == '__main__':
     # Создание экземпляра и запуск игры.
     ai = AlienInvasion()
